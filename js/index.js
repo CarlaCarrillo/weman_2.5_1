da7493c1f281c64279dced323ddb2ac8e9411a2c
CarlaCarrillo <ccarrillo1616@gmail.com>
2019-05-09 15:32:21 -0500

    tercera modificación al ejercicio

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ let ctrlMostrado = document.getElementById('salida');
 
 function dibujaTarjetas(arreglo) {
 	let imprime = "<table id='registro'><tr><th>Habitación</th><th>Huesped</th></tr>";
-	//La siguiente estructura de for es casi ya un arcaísmo. Sin embargo, los clásicos nunca mueren.
 	for (let i = 0; i < arreglo.length; i++) {
 		imprime += "<tr><td>" + arreglo[i].numHabitacion + "</td><td>" + arreglo[i].nombreHuesped + "</td></tr>";
 	}
@@ -12,52 +11,47 @@ function dibujaTarjetas(arreglo) {
 }
 
 function ordenaPorHabitacion(arreglo) {
-	var registro = [
-	{numHabitacion: "87", nombreHuesped: "Donna Noble"},
-	{numHabitacion: "34", nombreHuesped: "Jean Luc Piccard"},
-	{numHabitacion: "23", nombreHuesped: "Deana Troi"},
-	{numHabitacion: "12", nombreHuesped: "Christpher Pike"},
-	{numHabitacion: "67", nombreHuesped: "Amy Pond"},
-	{numHabitacion: "45", nombreHuesped: "Miyamoto Musashi"},
-	{numHabitacion: "93", nombreHuesped: "Amelia Earhart"},
-	{numHabitacion: "47", nombreHuesped: "Katherin Janeway"}
-];
-console.log(registro.sort(function (prev,next) {
-if (prev.numHabitacion > next.numHabitacion) {
-return 1;
+	//ordena los numero de habitación
+	console.log(registro.sort(function (ant, sig) {
+		//especificamos la propiedad a ordenar
+		if (ant.numHabitacion > sig.numHabitacion) {
+			return 1;
+		}
+		if (ant.numHabitacion < sig.numHabitacion) {
+			return -1;
+		}
+		return 0;
+	}));
+	//con esto "dibujarTarjetas" se imprimen
+	dibujaTarjetas(registro); 
 }
-if (prev.numHabitacion < next.numHabitacion) {
-return -1;
-}
-return 0;
-}));
-
-let ctrlMostrado = document.getElementById('salida');
-
-	//En esta función escribe la función que ordenará el arreglo por habitación
-}
-
 
 function ordenaPorHuesped(arreglo) {
-	var registro = [
-	{numHabitacion: "87", nombreHuesped: "Donna Noble"},
-	{numHabitacion: "34", nombreHuesped: "Jean Luc Piccard"},
-	{numHabitacion: "23", nombreHuesped: "Deana Troi"},
-	{numHabitacion: "12", nombreHuesped: "Christpher Pike"},
-	{numHabitacion: "67", nombreHuesped: "Amy Pond"},
-	{numHabitacion: "45", nombreHuesped: "Miyamoto Musashi"},
-	{numHabitacion: "93", nombreHuesped: "Amelia Earhart"},
-	{numHabitacion: "47", nombreHuesped: "Katherin Janeway"}
-];
-console.log(registro.sort(function (prev,next) {
-if (prev.nombreHuesped > next.nombreHuesped) {
-return 1;
-}
-if (prev.nombreHuesped < next.nombreHuesped) {
-return -1;
-}
-return 0;
-}));
-	//Acá puedes hacer la otra que ordene por el nombre del huesped
+	//trae (imprime) la info de 'registro" ya ordenada
+	console.log(registro.sort(function (ant, sig) {
+		//se tiene que establecer la propiedad a ordenar
+		if (ant.nombreHuesped > sig.nombreHuesped) {
+			return 1;
+		}
+		if (ant.nombreHuesped < sig.nombreHuesped) {
+			return -1;
+		}
+		return 0;
+	}));
+	dibujaTarjetas(registro);
 }
 
+//con esta función guardamos la información
+function guardar(){ 
+    var _habit = document.getElementById("habitacion").value;
+    var _huesp = document.getElementById("huesped").value;
+    // los muestra en la tabla... esta creando otra tabla?
+    var fila="<tr><td>"+_habit+"</td><td>"+_huesp +"</td></tr>";
+    // var _nuevoregistro = {numHabitacion:"110",...}  
+    
+    var btn = document.createElement("TR");
+    var nuevohuesped = {numHabitacion:_habit, nombreHuesped:_huesp};
+    btn.innerHTML=fila;
+    document.getElementById("registro").appendChild(btn);
+    registro.push(nuevohuesped);  
+}
